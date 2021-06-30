5755d547c75181288fa26116a927ae09de0b4ca7
Tate Hingston <86576306+TateHingston12345@users.noreply.github.com>
2021-06-30 01:54:33 +0000

    put inside function

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 import csv
-with open('countries.csv') as csv_file:
-  csv_reader = csv.reader(csv_file, delimiter=',')
-  line_count = 0
-  r = input('region: ')
-  print('------------')
-  while r != 'quit':
-    for row in csv_reader:
-      if r in f'{row[1]}':
-        print(f'{row[0]}')
-        print(f'{row[1]}')
-        print(f'{row[2]}')
-        print('------------')
+def region():
+  with open('countries.csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
     r = input('region: ')
     print('------------')
-  print('program quit')
-  print('------------')
+    while r != 'quit':
+      for row in csv_reader:
+        if r in f'{row[1]}':
+          print(f'{row[0]}')
+          print(f'{row[1]}')
+          print(f'{row[2]}')
+          print('------------')
+      r = input('region: ')
+      print('------------')
+    print('program quit')
+    print('------------')
+region()
